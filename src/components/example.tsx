c6dbd17afdcad11a39907e6228e2b625f6516030
HiagoSilva <hiagosilvaanjos7@gmail.com>
2026-02-22 13:19:30 -0300

    feat: add examples files

---
+++ src/components/example.tsx
@@ -0,0 +1,7 @@
+export function Example() {
+    return (
+        <div>
+            <h1>Exemplo</h1>
+        </div>
+    )
+}
---
+++ src/utils/time-format.ts
@@ -0,0 +1,9 @@
+// Função para formatar data e hora
+
+interface Date {
+    // ...
+}
+
+export function formatDateTime(date: Date): string {
+    return "Data e hora formatada"
+}
