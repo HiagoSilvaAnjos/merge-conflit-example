840f1207942b934d81ad32aa7ca9c44f20347569
HiagoSilva <hiagosilvaanjos7@gmail.com>
2026-02-22 13:09:43 -0300

    feat: initial files

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+
+function App() {
+
+  return (
+    <div className='flex flex-col items-center justify-center h-screen'>
+      <h1 className='text-3xl mt-10 font-bold underline w-full text-center'>Simulação de Merge Conflict</h1>
+      <p className='text-center mt-5 font-bold'>Projeto React com TailwindCSS</p>
+    </div>
+  )
+}
+
+export default App
